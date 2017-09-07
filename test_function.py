0aa009fd87ee33b00af12a1e5299e8fcae72d098
NinjMenon <ns229@duke.edu>
2017-09-07 16:46:35 -0400

    Corrected testing algorithm

--- test_function.py
+++ test_function.py
@@ -1,7 +1,7 @@
 def testfunc():
     import lec03script
-    assert lec03script(1)(1) == 2
-    assert lec03script(-2)(1) == 0
-    assert lec03script(0)(0) == 0
-    assert lec03script(2.5)(3.2) ==5.7
+    assert lec03script(1,1) == 2
+    assert lec03script(-2,1) == 0
+    assert lec03script(0,0) == 0
+    assert lec03script(2.5,3.2) ==5.7
 
