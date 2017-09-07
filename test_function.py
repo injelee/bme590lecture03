dd99d3f2d1080d5e2f783161c6cffbc67b3cd589
NinjMenon <ns229@duke.edu>
2017-09-07 16:37:33 -0400

    Modified the function

--- test_function.py
+++ test_function.py
@@ -1,5 +1,5 @@
 def testfunc():
-    from bme590lecture03 import lec03script
+    import lec03script
     assert lec03script(1)(1) == 2
     assert lec03script(-2)(1) == 0
     assert lec03script(0)(0) == 0
